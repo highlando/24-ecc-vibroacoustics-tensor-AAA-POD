84547e3de8f19789355f95d24123edaf6f723312
Jan Heiland <jnhlnd@gmail.com>
2024-11-09 18:20:03 +0100

    trying automation

--- matlab/AAA_tensor_approximation_tol.m
+++ matlab/AAA_tensor_approximation_tol.m
@@ -13,11 +13,16 @@
 %
 
 
-clear all; close all;
+close all;
 
 podtnsdata = load('../data/podtnsrdata.mat');
 
-largeorsmall = 3;
+if exist('clsp')
+  largeorsmall = clsp;
+else
+  clear all;
+  largeorsmall = 3;
+end
 
 ftnsr = podtnsdata.full;  % the full data
 
@@ -26,16 +31,20 @@ switch largeorsmall
     dtnsr = podtnsdata.pod3x3.dttnsr_ot;
     dvcso = podtnsdata.pod3x3.svdvecs_o;
     dvcsot = podtnsdata.pod3x3.svdvecs_ot;
+    nx = 3;
   case 2
     dtnsr = podtnsdata.pod6x6.dttnsr_ot;
     dvcso = podtnsdata.pod6x6.svdvecs_o;
     dvcsot = podtnsdata.pod6x6.svdvecs_ot;
+    nx = 6;
   case 3
     dtnsr = podtnsdata.pod12x12.dttnsr_ot;
     dvcso = podtnsdata.pod12x12.svdvecs_o;
     dvcsot = podtnsdata.pod12x12.svdvecs_ot;
+    nx = 12;
   case 4 
     dtnsr = ftnsr;  
+    nx = 21;
 end
 
 pts = (10:400);
@@ -47,47 +56,56 @@ end
 size(FF)
 
 % set tolerance here
-%opts.tol = 1e-4;
-opts.tol = 1e-6;
+% opts.tol = 1e-6;
 % opts.tol = 1e-8;
 %opts.tol = 1e-10;
 
-% set maximum numbe rof iterations (100 to make sure it stops at the
-% tolerancevalue)
-opts.maxit = 100;
+tolvec = [1e-4, 1e-6, 1e-8, 1e-10];
 
-opts.return = 'best';
+for ctol = tolvec
+  opts.tol = ctol;
 
-% this is Block-AAA
-tic;[R1,rmse1,out1] = block_aaa(FF,pts,opts); toc
+  % set maximum numbe rof iterations (100 to make sure it stops at the
+  % tolerancevalue)
+  opts.maxit = 100;
 
-ord1 = length(out1.zk)
+  opts.return = 'best';
 
-% this is SetValued-AAA
-tic;[R2,rmse2,zk2] = set_val_aaa(FF,pts,opts.maxit,opts.tol);toc
+  % this is Block-AAA
+  tic;[R1,rmse1,out1] = block_aaa(FF,pts,opts); toc
 
-ord2 = length(zk2)
+  ord1 = length(out1.zk)
 
+  % this is SetValued-AAA
+  tic;[R2,rmse2,zk2] = set_val_aaa(FF,pts,opts.maxit,opts.tol);toc
 
-disp(['EXAMPLE 1 - best RMSE achieved: ' num2str(rmse(pts,R1,FF)) ])
-figure, semilogy(0:length(rmse1)-1,rmse1), hold on
-semilogy([0,opts.maxit-1],opts.tol*[1,1],'k--');
-semilogy(0:length(rmse2)-1,rmse2,'-.r');
-xlabel('degree'), ylabel('RMSE')
-legend('Block-AAA convergence','Set-valued AAA convergence')
+  ord2 = length(zk2)
 
-%Here are the interpolation points (frequencies) that the algorithm
-%Block-AAA selects
-selected_pts = out1.zk.';
+  %Here are the interpolation points (frequencies) that the algorithm
+  %Block-AAA selects
+  selected_pts = out1.zk.';
 
-for ii = 1:length(pts)
-%     err1(ii)= norm(FF{ii}-R1(pts(ii)));
-%     err2(ii)= norm(FF{ii}-R2(pts(ii)));
-    Fap1 = dvcso * R1(pts(ii)) * dvcsot';
-    Fap2 = dvcso * R2(pts(ii)) * dvcsot';
-    Fex = ftnsr(:, :, ii);
-    err1(ii)= norm(Fex-Fap1, 'fro');
-    err2(ii)= norm(Fex-Fap2, 'fro');
+  for ii = 1:length(pts)
+  %     err1(ii)= norm(FF{ii}-R1(pts(ii)));
+  %     err2(ii)= norm(FF{ii}-R2(pts(ii)));
+      Fap1 = dvcso * R1(pts(ii)) * dvcsot';
+      Fap2 = dvcso * R2(pts(ii)) * dvcsot';
+      Fex = ftnsr(:, :, ii);
+      err1(ii)= norm(Fex-Fap1, 'fro');
+      err2(ii)= norm(Fex-Fap2, 'fro');
+  end
+
+  err1 = zeros(length(pts), 1);
+  err2 = zeros(length(pts), 1);
+  for ii = 1:length(pts)
+      app1 = dvcso * R1(pts(ii)) * dvcsot';
+      app2 = dvcso * R2(pts(ii)) * dvcsot';
+      err1(ii) = norm(ftnsr(:, :, ii)-app1, 'fro');
+      err2(ii) = norm(ftnsr(:, :, ii)-app2, 'fro');
+  end
+  fprintf('blk: nx: %d, tol: %e, err: %.2e', nx, ctol, norm(err1))
+  fprintf('set: nx: %d, tol: %e, err: %.2e', nx, ctol, norm(err2))
+
 end
 
 figure
@@ -97,14 +115,9 @@ legend('Block-AAA','SetValued-AAA');
 ylabel('Error (2-norm of the deviation)');
 xlabel('Frequency');
 
-err1 = zeros(length(pts), 1);
-err2 = zeros(length(pts), 1);
-for ii = 1:length(pts)
-    app1 = dvcso * R1(pts(ii)) * dvcsot';
-    app2 = dvcso * R2(pts(ii)) * dvcsot';
-    err1(ii) = norm(ftnsr(:, :, ii)-app1, 'fro');
-    err2(ii) = norm(ftnsr(:, :, ii)-app2, 'fro');
-end
-norm(err1)
-norm(err2)
-
+% disp(['EXAMPLE 1 - best RMSE achieved: ' num2str(rmse(pts,R1,FF)) ])
+figure, semilogy(0:length(rmse1)-1,rmse1), hold on
+semilogy([0,opts.maxit-1],opts.tol*[1,1],'k--');
+semilogy(0:length(rmse2)-1,rmse2,'-.r');
+xlabel('degree'), ylabel('RMSE')
+legend('Block-AAA convergence','Set-valued AAA convergence')
